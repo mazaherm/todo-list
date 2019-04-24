0dcd43a5484a704feca5b601f901840d0597a5b9
Mazaher Muraj <mmuraj.313@gmail.com>
2019-04-25 00:16:42 +0100

    toggles list item that has been checked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 /**
  * This strikes through list item on click once and item is marked as done
  */
-const listItem = document.querySelectorAll('li')
+const listItem = document.getElementsByTagName('li')
 
 for (let i = 0; i < listItem.length; i++) {
   listItem[i].addEventListener('click', () => {
-    listItem[i] = listItem[i].style.setProperty('text-decoration', 'line-through')
+    listItem[i].classList.toggle('checked')
   })
 }
