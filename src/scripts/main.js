83edde05842b93b98bcb632ce49b1c229b3ed004
Mazaher Muraj <mmuraj.313@gmail.com>
2019-04-26 23:49:50 +0100

    refactors checked item function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 /**
  * This strikes through list item on click once and item is marked as done
  */
-const listItem = document.getElementsByTagName('li')
-
-for (let i = 0; i < listItem.length; i++) {
-  listItem[i].addEventListener('click', () => {
-    listItem[i].classList.toggle('checked')
-  })
+const checkItem = (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('checked')
+  }
 }
+
+document.addEventListener('click', checkItem)
 
 /**
  * This deletes an item from the list on click
